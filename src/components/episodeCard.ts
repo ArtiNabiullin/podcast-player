aab6d9dc3c83af9fed0d79fa5a1914c2d857c385
ArtiNabiullin <anabiullin05@gmail.com>
2026-07-09 18:53:23 +0500

    feat: add createEpisodeCard function

--- src/components/episodeCard.ts
+++ src/components/episodeCard.ts
@@ -3,11 +3,17 @@ import type { Episode } from "../types/podcast";
 //компонент карточки эпизода
 export function createEpisodeCard(episode: Episode): HTMLDivElement {
   const card = document.createElement("div");
-
   card.className = "episode-card";
+
+  const image = document.createElement("img");
+  image.src = episode.image;
+  image.alt = episode.title;
 
   const title = document.createElement("h3");
   title.textContent = episode.title;
+
+  const date = document.createElement("p");
+  date.textContent = new Date(episode.pubDateMs).toLocaleDateString();
 
   const duration = document.createElement("p");
   duration.textContent = `${Math.floor(episode.audioLengthSec / 60)} min`;
@@ -19,7 +25,7 @@ export function createEpisodeCard(episode: Episode): HTMLDivElement {
   audio.controls = true;
   audio.src = episode.audio;
 
-  card.append(title, duration, description, audio);
+  card.append(image, title, date, duration, description, audio);
 
   return card;
 }
