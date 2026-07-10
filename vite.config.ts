d9462510715f29081e342399f6ffa10c87e26217
ArtiNabiullin <anabiullin05@gmail.com>
2026-07-10 18:26:44 +0500

    feat: fix vite error

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/podcast-player/" : "/",
+  base: "/",
 });
